0f6efca6c72987086538b6e7fe80b56b3be37263
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-11 20:00:59 -0300

    522-530-580: Criação da Classe Formation.

---
+++ GwiNews/GwiNews.Domain/Entities/Formation.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GwiNews.Domain.Entities
+{
+    public class Formation
+    {
+        [Key]
+        public Guid? Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string? Institution { get; set; }
+        [Required]
+        public DateTime? StartDate { get; set; }
+        [Required]
+        public DateTime? EndDate { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string? Activity1 { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string? Activity2 { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string? Activity3 { get; set; }
+        [Required]
+        public Guid? UserId { get; set; }
+        [Required]
+        public ReaderUser? User { get; set; }
+    }
+}
