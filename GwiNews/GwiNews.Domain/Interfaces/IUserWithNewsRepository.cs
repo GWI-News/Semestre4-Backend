33bc54c8ba4cd75454e65003acb8de005f4ee1dc
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-12 17:34:32 -0300

    522-531-586: UserWithNews Repository e Repository Interface.

---
+++ GwiNews/GwiNews.Domain/Interfaces/IUserWithNewsRepository.cs
@@ -0,0 +1,14 @@
+﻿using GwiNews.Domain.Entities;
+
+namespace GwiNews.Domain.Interfaces
+{
+    public interface IUserWithNewsRepository
+    {
+        public Task<IEnumerable<UserWithNews>> GetUsersWithNews();
+        public Task<UserWithNews> GetUserWithNews(Guid id);
+        public Task<IEnumerable<UserWithNews>> GetActiveUsersWithNews();
+        public Task<UserWithNews> CreateUserWithNews(UserWithNews userWithNews);
+        public Task<UserWithNews> UpdateUserWithNews(UserWithNews userWithNews);
+        public Task<IEnumerable<UserWithNews>> DeleteUserWithNews(UserWithNews userWithNews);
+    }
+}
