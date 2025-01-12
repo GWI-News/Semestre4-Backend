fd08c029109817d350cdd63511c3383668db85bd
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-11 22:37:13 -0300

    522-534-584: DbSet e EntityConfiguration para Formation.

---
+++ GwiNews/GwiNews.Infra.Data/EntityConfiguration/FormationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using GwiNews.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GwiNews.Infra.Data.EntityConfiguration
+{
+    public class FormationConfiguration : IEntityTypeConfiguration<Formation>
+    {
+        public void Configure(EntityTypeBuilder<Formation> builder)
+        {
+            builder.HasKey(f => f.Id);
+            builder.Property(f => f.Name).IsRequired().HasMaxLength(255);
+            builder.Property(f => f.Institution).IsRequired().HasMaxLength(255);
+            builder.Property(f => f.StartDate).IsRequired();
+            builder.Property(f => f.EndDate).IsRequired();
+            builder.Property(f => f.Activity1).IsRequired().HasMaxLength(255);
+            builder.Property(f => f.Activity2).IsRequired().HasMaxLength(255);
+            builder.Property(f => f.Activity3).IsRequired().HasMaxLength(255);
+        }
+    }
+}
