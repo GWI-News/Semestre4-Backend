5b65c4d2b660823d89db42977f312c9a48c75551
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-12 23:59:30 -0300

    522-531-591: Interface e Classe Professional Information Repository.

---
+++ GwiNews/GwiNews.Domain/Interfaces/IProfessionalInformationRepository.cs
@@ -0,0 +1,15 @@
+﻿using GwiNews.Domain.Entities;
+
+namespace GwiNews.Domain.Interfaces
+{
+    public interface IProfessionalInformationRepository
+    {
+        public Task<IEnumerable<ProfessionalInformation>> GetProfessionalInformations();
+        public Task<ProfessionalInformation> GetProfessionalInformation(Guid id);
+        public Task<IEnumerable<ProfessionalInformation>> GetActiveProfessionalInformations();
+        public Task<IEnumerable<ProfessionalInformation>> GetProfessionalInformationsByUserId(Guid userId);
+        public Task<ProfessionalInformation> CreateProfessionalInformation(ProfessionalInformation professionalInformation);
+        public Task<ProfessionalInformation> UpdateProfessionalInformation(ProfessionalInformation professionalInformation);
+        public Task<IEnumerable<ProfessionalInformation>> DeleteProfessionalInformation(ProfessionalInformation professionalInformation);
+    }
+}
---
+++ GwiNews/GwiNews.Infra.Data/Repository/ProfessionalInformationRepository.cs
@@ -0,0 +1,111 @@
+﻿using GwiNews.Domain.Entities;
+using GwiNews.Domain.Interfaces;
+using GwiNews.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GwiNews.Infra.Data.Repository
+{
+    public class ProfessionalInformationRepository : IProfessionalInformationRepository
+    {
+        private readonly AppDbContext _context;
+        public ProfessionalInformationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ProfessionalInformation>> GetProfessionalInformations()
+        {
+            try
+            {
+                var professionalInformations = await _context.ProfessionalInformations.ToListAsync();
+                return professionalInformations;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<ProfessionalInformation> GetProfessionalInformation(Guid id)
+        {
+            try
+            {
+                var professionalInformation = await _context.ProfessionalInformations.FindAsync(id);
+                return professionalInformation;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<ProfessionalInformation>> GetActiveProfessionalInformations()
+        {
+            try
+            {
+                var professionalInformations = await _context.ProfessionalInformations.Where(u => u.Status == true).ToListAsync();
+                return professionalInformations;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<ProfessionalInformation>> GetProfessionalInformationsByUserId(Guid userId)
+        {
+            try
+            {
+                var professionalInformations = await _context.ProfessionalInformations.Where(u => u.Status == true && u.Id == userId).ToListAsync();
+                return professionalInformations;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<ProfessionalInformation> CreateProfessionalInformation(ProfessionalInformation professionalInformation)
+        {
+            try
+            {
+                _context.ProfessionalInformations.Add(professionalInformation);
+                await _context.SaveChangesAsync();
+                return professionalInformation;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<ProfessionalInformation> UpdateProfessionalInformation(ProfessionalInformation professionalInformation)
+        {
+            try
+            {
+                _context.Update(professionalInformation);
+                await _context.SaveChangesAsync();
+                return professionalInformation;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<ProfessionalInformation>> DeleteProfessionalInformation(ProfessionalInformation professionalInformation)
+        {
+            try
+            {
+                professionalInformation.Status = false;
+                _context.Update(professionalInformation);
+                await _context.SaveChangesAsync();
+                return await _context.ProfessionalInformations.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
