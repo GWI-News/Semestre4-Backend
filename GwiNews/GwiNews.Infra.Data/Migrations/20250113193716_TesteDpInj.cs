6eedb7cd03b5758e0c7c9078fceb5efc64169eda
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-13 16:45:17 -0300

    522-536-594: Classe Dependency Injection API.

---
+++ GwiNews/GwiNews.Infra.Data/Migrations/20250113193716_TesteDpInj.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GwiNews.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TesteDpInj : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "ProfessionalSkills",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "ProfessionalInformations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "NewsSubcategories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "NewsCategories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Formations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ProfessionalSkills");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ProfessionalInformations");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "NewsSubcategories");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "NewsCategories");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Formations");
+        }
+    }
+}
---
+++ GwiNews/GwiNews.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,29 @@
+﻿using GwiNews.Domain.Interfaces;
+using GwiNews.Infra.Data.Context;
+using GwiNews.Infra.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GwiNews.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(option =>
+                option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUserWithNewsRepository, UserWithNewsRepository>();
+            services.AddScoped<INewsRepository, NewsRepository>();
+            services.AddScoped<INewsCategoryRepository, NewsCategoryRepository>();
+            services.AddScoped<INewsSubcategoryRepository, NewsSubcategoryRepository>();
+            services.AddScoped<IReaderUserRepository, ReaderUserRepository>();
+            services.AddScoped<IProfessionalInformationRepository, ProfessionalInformationRepository>();
+            services.AddScoped<IProfessionalSkillRepository, ProfessionalSkillRepository>();
+            services.AddScoped<IFormationRepository, FormationRepository>();
+
+            return services;
+        }
+    }
+}
