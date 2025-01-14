88e39a536663b3c621d6360813575e97b56b79d1
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-14 17:44:50 -0300

    523-537-599: DTOs Create e Update de Reader User.

---
+++ GwiNews/GwiNews.Application/DTOs/ReaderUser/CreateReaderUserDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace GwiNews.Application.DTOs.ReaderUser
+{
+    public class CreateReaderUserDTO
+    {
+        public UserRole? Role { get; set; }
+        public string? CompleteName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool? Status { get; set; }
+    }
+
+    public enum UserRole
+    {
+        Administrator = 0,
+        Editor = 1,
+        Author = 2,
+        Reader = 3,
+        Marketing = 4,
+        Financial = 5
+    }
+}
---
+++ GwiNews/GwiNews.Application/DTOs/ReaderUser/UpdateReaderUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace GwiNews.Application.DTOs.ReaderUser
+{
+    public class UpdateReaderUserDTO
+    {
+        public Guid? Id { get; set; }
+        public UserRole? Role { get; set; }
+        public string? CompleteName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool? Status { get; set; }
+    }
+}
