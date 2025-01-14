3f89be706a5b9b2d19e5333db1bec9aafb0d4e7f
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-14 18:00:51 -0300

    523-537-601: DTOs Create e Update de Professional Skill.

---
+++ GwiNews/GwiNews.Application/DTOs/ProfessionalSkill/UpdateProfessionalSkillDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace GwiNews.Application.DTOs.ProfessionalSkill
+{
+    public class UpdateProfessionalSkillDTO
+    {
+        public Guid? Id { get; set; }
+        public string? Skill1 { get; set; }
+        public string? Skill2 { get; set; }
+        public string? Skill3 { get; set; }
+        public string? Skill4 { get; set; }
+        public bool? Status { get; set; }
+        public Guid? UserId { get; set; }
+    }
+}
