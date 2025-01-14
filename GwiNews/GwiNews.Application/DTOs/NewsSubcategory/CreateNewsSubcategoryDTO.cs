273596731788be5cad439c235b8652ffdc12634b
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-14 17:04:03 -0300

    523-537-598: DTOs Create e Update de News Subcategory.

---
+++ GwiNews/GwiNews.Application/DTOs/NewsSubcategory/CreateNewsSubcategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GwiNews.Application.DTOs.NewsSubcategory
+{
+    public class CreateNewsSubcategoryDTO
+    {
+        public string? Name { get; set; }
+        public bool? Status { get; set; }
+        public Guid? CategoryId { get; set; }
+    }
+}
