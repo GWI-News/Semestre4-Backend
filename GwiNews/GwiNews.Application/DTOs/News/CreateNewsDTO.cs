741b99479d488b5288385cb49d3f5071898c2710
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-14 16:38:22 -0300

    523-537-596: DTOs Create e Update de News.

---
+++ GwiNews/GwiNews.Application/DTOs/News/CreateNewsDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace GwiNews.Application.DTOs.News
+{
+    public class CreateNewsDTO
+    {
+        public NewsStatus? Status { get; set; }
+        public string? NewsUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Subtitle { get; set; }
+        public string? NewsBody { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? PublishDate { get; set; }
+        public Guid? AuthorId { get; set; }
+        public Guid? EditorId { get; set; }
+        public Guid? CategoryId { get; set; }
+        public ICollection<Guid>? Subcategories { get; set; }
+    }
+
+    public enum NewsStatus
+    {
+        Published = 0,
+        InRevision = 1,
+        Draft = 2,
+        Inactive = 3
+    }
+}
