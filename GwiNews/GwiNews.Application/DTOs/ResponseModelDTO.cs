8f2e710ffa907f87d14e09bfea666da92bffed8d
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-15 16:39:45 -0300

    523-537-605: Response Model DTO.

---
+++ GwiNews/GwiNews.Application/DTOs/ResponseModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GwiNews.Application.DTOs
+{
+    public class ResponseModelDTO<T>
+    {
+        public T? Data { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool Status { get; set; } = true;
+    }
+}
