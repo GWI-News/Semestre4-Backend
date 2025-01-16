29979e5324aa0ff1c08cbbbf3f91875def0e64a9
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-16 16:14:58 -0300

    523-539-611: Interface e Classe News Category Service.

---
+++ GwiNews/GwiNews.Application/Interfaces/INewsCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using GwiNews.Application.DTOs;
+using GwiNews.Application.DTOs.NewsCategory;
+using GwiNews.Domain.Entities;
+
+namespace GwiNews.Application.Interfaces
+{
+    public interface INewsCategoryService
+    {
+        Task<ResponseModelDTO<IEnumerable<NewsCategory>>> GetNewsCategories();
+        Task<ResponseModelDTO<NewsCategory>> GetNewsCategory(Guid id);
+        Task<ResponseModelDTO<IEnumerable<NewsCategory>>> GetActiveNewsCategories();
+        Task<ResponseModelDTO<NewsCategory>> CreateNewsCategory(CreateNewsCategoryDTO createNewsCategoryDTO);
+        Task<ResponseModelDTO<NewsCategory>> UpdateNewsCategory(UpdateNewsCategoryDTO updateNewsCategoryDTO);
+        Task<ResponseModelDTO<IEnumerable<NewsCategory>>> DeleteNewsCategory(UpdateNewsCategoryDTO updateNewsCategoryDTO);
+    }
+}
---
+++ GwiNews/GwiNews.Application/Services/NewsCategoryService.cs
@@ -0,0 +1,132 @@
+﻿using AutoMapper;
+using GwiNews.Application.DTOs;
+using GwiNews.Application.DTOs.NewsCategory;
+using GwiNews.Application.Interfaces;
+using GwiNews.Domain.Entities;
+using GwiNews.Domain.Interfaces;
+
+namespace GwiNews.Application.Services
+{
+    public class NewsCategoryService : INewsCategoryService
+    {
+        private readonly INewsCategoryRepository _newsCategoryRepository;
+        private readonly IMapper _mapper;
+
+        public NewsCategoryService(INewsCategoryRepository newsCategoryRepository, IMapper mapper)
+        {
+            _newsCategoryRepository = newsCategoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseModelDTO<IEnumerable<NewsCategory>>> GetNewsCategories()
+        {
+            ResponseModelDTO<IEnumerable<NewsCategory>> response = new ResponseModelDTO<IEnumerable<NewsCategory>>();
+            try
+            {
+                var newsCategories = await _newsCategoryRepository.GetNewsCategories();
+                response.Data = newsCategories;
+                response.Message = "Categorias de Notícias Listadas com Sucesso.";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
+        }
+
+        public async Task<ResponseModelDTO<NewsCategory>> GetNewsCategory(Guid id)
+        {
+            ResponseModelDTO<NewsCategory> response = new ResponseModelDTO<NewsCategory>();
+            try
+            {
+                var newsCategory = await _newsCategoryRepository.GetNewsCategory(id);
+                response.Data = newsCategory;
+                response.Message = "Categoria de Notícia Listada com Sucesso.";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
+        }
+
+        public async Task<ResponseModelDTO<IEnumerable<NewsCategory>>> GetActiveNewsCategories()
+        {
+            ResponseModelDTO<IEnumerable<NewsCategory>> response = new ResponseModelDTO<IEnumerable<NewsCategory>>();
+            try
+            {
+                var newsCategories = await _newsCategoryRepository.GetActiveNewsCategories();
+                response.Data = newsCategories;
+                response.Message = "Categorias de Notícias Ativas Listadas com Sucesso.";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
+        }
+
+        public async Task<ResponseModelDTO<NewsCategory>> CreateNewsCategory(CreateNewsCategoryDTO createNewsCategoryDTO)
+        {
+            ResponseModelDTO<NewsCategory> response = new ResponseModelDTO<NewsCategory>();
+            try
+            {
+                var newsCategory = _mapper.Map<NewsCategory>(createNewsCategoryDTO);
+                newsCategory = await _newsCategoryRepository.CreateNewsCategory(newsCategory);
+                response.Data = newsCategory;
+                response.Message = "Categoria de Notícia Criada com Sucesso.";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
+        }
+
+        public async Task<ResponseModelDTO<NewsCategory>> UpdateNewsCategory(UpdateNewsCategoryDTO updateNewsCategoryDTO)
+        {
+            ResponseModelDTO<NewsCategory> response = new ResponseModelDTO<NewsCategory>();
+            try
+            {
+                var newsCategory = _mapper.Map<NewsCategory>(updateNewsCategoryDTO);
+                newsCategory = await _newsCategoryRepository.UpdateNewsCategory(newsCategory);
+                response.Data = newsCategory;
+                response.Message = "Categoria de Notícia Atualizada com Sucesso.";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
+        }
+
+        public async Task<ResponseModelDTO<IEnumerable<NewsCategory>>> DeleteNewsCategory(UpdateNewsCategoryDTO updateNewsCategoryDTO)
+        {
+            ResponseModelDTO<IEnumerable<NewsCategory>> response = new ResponseModelDTO<IEnumerable<NewsCategory>>();
+            try
+            {
+                var newsCategory = _mapper.Map<NewsCategory>(updateNewsCategoryDTO);
+                var newsCategories = await _newsCategoryRepository.DeleteNewsCategory(newsCategory);
+                response.Data = newsCategories;
+                response.Message = "Categoria de Notícia Excluída com Sucesso.";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
+        }
+    }
+}
