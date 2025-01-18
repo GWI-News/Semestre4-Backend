44edf80dc02fd8c54185125577e776bf15695510
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-17 22:58:58 -0300

    523-539-624: Interface e Classe Professional Skill Service.

--- GwiNews/GwiNews.Domain/Interfaces/IProfessionalSkillRepository.cs
+++ GwiNews/GwiNews.Domain/Interfaces/IProfessionalSkillRepository.cs
@@ -8,6 +8,7 @@ namespace GwiNews.Domain.Interfaces
         public Task<ProfessionalSkill> GetProfessionalSkill(Guid id);
         public Task<IEnumerable<ProfessionalSkill>> GetActiveProfessionalSkills();
         public Task<IEnumerable<ProfessionalSkill>> GetProfessionalSkillsByUserId(Guid userId);
+        public Task<IEnumerable<ProfessionalSkill>> GetActiveProfessionalSkillsByUserId(Guid userId);
         public Task<ProfessionalSkill> CreateProfessionalSkill(ProfessionalSkill professionalSkill);
         public Task<ProfessionalSkill> UpdateProfessionalSkill(ProfessionalSkill professionalSkill);
         public Task<IEnumerable<ProfessionalSkill>> DeleteProfessionalSkill(ProfessionalSkill professionalSkill);
