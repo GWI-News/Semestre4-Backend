97ca52cb6601a0c7cdaf5f462758343bc136d210
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-19 14:09:48 -0300

    523-539-620: Hot Fix em Interface e Classe Professional Information Service.

--- GwiNews/GwiNews.Application/Interfaces/IProfessionalInformationService.cs
+++ GwiNews/GwiNews.Application/Interfaces/IProfessionalInformationService.cs
@@ -6,11 +6,11 @@ namespace GwiNews.Application.Interfaces
 {
     public interface IProfessionalInformationService
     {
-        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetProfessionaInformations();
-        Task<ResponseModelDTO<ProfessionalInformation>> GetProfessionaInformation(Guid id);
-        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetActiveProfessionaInformations();
-        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetProfessionaInformationsByUserId(Guid id);
-        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetActiveProfessionaInformationsByUserId(Guid id);
+        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetProfessionalInformations();
+        Task<ResponseModelDTO<ProfessionalInformation>> GetProfessionalInformation(Guid id);
+        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetActiveProfessionalInformations();
+        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetProfessionalInformationsByUserId(Guid id);
+        Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> GetActiveProfessionalInformationsByUserId(Guid id);
         Task<ResponseModelDTO<ProfessionalInformation>> CreateProfessionalInformation(CreateProfessionalInformationDTO createProfessionalInformationDTO);
         Task<ResponseModelDTO<ProfessionalInformation>> UpdateProfessionalInformation(UpdateProfessionalInformationDTO updateProfessionalInformationDTO);
         Task<ResponseModelDTO<IEnumerable<ProfessionalInformation>>> DeleteProfessionalInformation(UpdateProfessionalInformationDTO updateProfessionalInformationDTO);
