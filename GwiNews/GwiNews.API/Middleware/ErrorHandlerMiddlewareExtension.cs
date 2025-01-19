704a0c249d19d7c0e43b82b23df42fff73a45024
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-19 14:46:11 -0300

    523-543-628: Middleware e Extension Middleware.

---
+++ GwiNews/GwiNews.API/Middleware/ErrorHandlerMiddlewareExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace GwiNews.API.Middleware
+{
+    public static class ErrorHandlerMiddlewareExtension
+    {
+        public static IApplicationBuilder UseErrorHandlerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorHandlerMiddleware>();
+        }
+    }
+}
