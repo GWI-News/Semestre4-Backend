ffeda6c3aec49e29b232e79c49ec722b3ae6f346
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-01-19 14:23:13 -0300

    523-539-624: Hot Fix Professional Skill Services.

--- GwiNews/GwiNews.Application/Interfaces/IProfessionalSkillService.cs
+++ GwiNews/GwiNews.Application/Interfaces/IProfessionalSkillService.cs
@@ -6,11 +6,11 @@ namespace GwiNews.Application.Interfaces
 {
     public interface IProfessionalSkillService
     {
-        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetProfessionaSkills();
-        Task<ResponseModelDTO<ProfessionalSkill>> GetProfessionaSkill(Guid id);
-        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetActiveProfessionaSkills();
-        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetProfessionaSkillsByUserId(Guid id);
-        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetActiveProfessionaSkillsByUserId(Guid id);
+        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetProfessionalSkills();
+        Task<ResponseModelDTO<ProfessionalSkill>> GetProfessionalSkill(Guid id);
+        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetActiveProfessionalSkills();
+        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetProfessionalSkillsByUserId(Guid id);
+        Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> GetActiveProfessionalSkillsByUserId(Guid id);
         Task<ResponseModelDTO<ProfessionalSkill>> CreateProfessionalSkill(CreateProfessionalSkillDTO createProfessionalSkillDTO);
         Task<ResponseModelDTO<ProfessionalSkill>> UpdateProfessionalSkill(UpdateProfessionalSkillDTO updateProfessionalSkillDTO);
         Task<ResponseModelDTO<IEnumerable<ProfessionalSkill>>> DeleteProfessionalSkill(UpdateProfessionalSkillDTO updateProfessionalSkillDTO);
